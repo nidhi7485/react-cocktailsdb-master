ef78e221d7c0f0d15757745c0533876c059057da
nidhi <nidhi7485@gmail.com>
2023-04-16 16:02:30 +0530

    completed

--- src/components/Cocktail.js
+++ src/components/Cocktail.js
@@ -1,5 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Cocktail({ id, name, info, glass, image }) {
-  return <h1>cocktail component</h1>
+  return (
+    <article className='cocktail'>
+      <div className='img-container'>
+        <img src={image} alt={name} />
+      </div>
+      <div className='cocktail-footer'>
+        <h3>{name}</h3>
+        <h4>{glass}</h4>
+        <p>{info}</p>
+        <Link to={`/cocktail/${id}`} className='btn btn-primary btn-details'>
+          details
+        </Link>
+      </div>
+    </article>
+  )
 }
